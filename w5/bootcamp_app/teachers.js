9ce83331dc7749ca480995e48401d4a5889166d5
aliy0012 <alikamal415@yahoo.com>
2021-11-24 23:12:55 +0000

    paramaetrized querry

--- w5/bootcamp_app/teachers.js
+++ w5/bootcamp_app/teachers.js
@@ -17,17 +17,27 @@ LIMIT 5;
 })
 .catch(err => console.error('query error', err.stack));
 
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = LIKE $1 || 'JUL02'}'
+LIMIT $2
 ORDER BY teacher;
-`)
+`;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
   })
+.catch((err) => console.log(err));
+.finally(() => pool.end());
 });  
